178a32bb35c552d66c8e92c859993aa7a9797e7c
Anatolli1989 <155433156+anatolli1989@users.noreply.github.com>
2024-01-03 00:51:25 +0300

    added conditions theory

--- hom2.py
+++ hom2.py
@@ -1,3 +1,58 @@
 print("Hello Woorld")
 print("Hello World")
 print("Hello qwerty")
+# умови
+# v1
+# n1 = 10
+# n2 = 20
+# v2
+# n1, n2 = 10, 20  # множинне привласнення
+# print(n1 > n2)
+# print(n1 >= n2)
+# print(n1 < n2)
+# print(n1 <= n2)
+# print(n1 == n2)  # поверне True якщо обидва операнди рівні (однакові)
+# print(n1 != n2)  # поверне True якщо обидва операнди різні
+# #
+# print(1 == 1 and 3 == 3)  # поверне True якщо обидва операнди рівні True, інакше False
+# print(1 == 1 or 2 == 3)  # поверне True якщо хоча б один операнд дорівнює True, інакше False
+# #
+# is_valid = False
+# print(is_valid)
+# print(not is_valid)  # not -> інверсія, якщо значення False стане True, і навпаки
+#
+# print("hello" in "hello world")
+
+###
+####
+# hours = int(input("Enter hours: "))
+
+# v1
+# if hours >= 12:
+#     print("PM")
+#     print("asdf")
+# else:
+#     print("AM")
+
+# v2
+# if 12 <= hours < 24:
+#     print("PM")
+# elif 0 <= hours < 12:
+#     print("AM")
+# else:
+#     print("Incorrect hours!")
+
+###############
+# ввести рейтинг фільму: якщо рейтинг дорівнює 5 або 4 - ок, інакше - погано
+
+film_rating = int(input("Enter film rating: "))
+
+if film_rating > 0 and film_rating <= 5:
+    if film_rating == 4 or film_rating == 5:
+        print("OK!")
+    else:
+        print("Not OK!")
+else:
+    print("Incorrect rating!")
+
+print("Hello world!")
